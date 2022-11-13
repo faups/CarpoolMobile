682a5a83f31631b4bdf901467d0706c19af253ec
Luke Faupel <lukefaupel@gmail.com>
2022-11-13 14:33:45 -0600

    added firebase write to registration

--- App.tsx
+++ App.tsx
@@ -1,7 +1,5 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
 
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
@@ -10,25 +8,6 @@ import Navigation from "./navigation";
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
-
-  const firebaseConfig = {
-    apiKey: "AIzaSyA37f8BDFALhLdLDPYasUpgSsop0-UyV8Q",
-    authDomain: "carpool-fullstack.firebaseapp.com",
-    projectId: "carpool-fullstack",
-    storageBucket: "carpool-fullstack.appspot.com",
-    messagingSenderId: "289458205081",
-    appId: "1:289458205081:web:007c8bdd52c52b10ebe308",
-    measurementId: "G-B6KW4JECHV",
-  };
-
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore(app);
-
-  const querySnapshot = getDocs(collection(db, "users")).then((result) => {
-    result.forEach((doc) => {
-      console.log(`${doc.id} => ${doc.data()}`);
-    });
-  });
 
   if (!isLoadingComplete) {
     return null;
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,17 +1,30 @@
-import React, { useState, useRef } from 'react';
-import { StyleSheet, TextInput, Button, ScrollView } from 'react-native';
+import React, { useState, useRef } from "react";
+import {
+  StyleSheet,
+  TextInput,
+  Button,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Platform,
+  Keyboard,
+  ScrollView,
+} from "react-native";
+import { initializeApp } from "firebase/app";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
+import { RootTabScreenProps } from "../types";
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
-  const [parent, setTextP] = useState('');
-  const [childrens, setTextC] = useState('');
-  const [make, setTextMake] = useState('');
-  const [model, setTextModel] = useState('');
-  const [liscense, setTextL] = useState('');
-  const [pool, setTextPool] = useState('');
+export default function TabOneScreen({
+  navigation,
+}: RootTabScreenProps<"TabOne">) {
+  const [parent, setTextP] = useState("");
+  const [childrens, setTextC] = useState("");
+  const [make, setTextMake] = useState("");
+  const [model, setTextModel] = useState("");
+  const [liscense, setTextL] = useState("");
+  const [pool, setTextPool] = useState("");
 
   const childInput = useRef(null);
   const makeInput = useRef(null);
@@ -19,81 +32,129 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
   const liscenseInput = useRef(null);
   const poolInput = useRef(null);
 
+  const firebaseConfig = {
+    apiKey: "AIzaSyA37f8BDFALhLdLDPYasUpgSsop0-UyV8Q",
+    authDomain: "carpool-fullstack.firebaseapp.com",
+    projectId: "carpool-fullstack",
+    storageBucket: "carpool-fullstack.appspot.com",
+    messagingSenderId: "289458205081",
+    appId: "1:289458205081:web:007c8bdd52c52b10ebe308",
+    measurementId: "G-B6KW4JECHV",
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+
   const submit = () => {
+    const docRef = addDoc(collection(db, "users"), {
+      parent,
+      childrens,
+      make,
+      model,
+      liscense,
+      pool,
+    });
+    setTextP("");
+    setTextC("");
+    setTextMake("");
+    setTextModel("");
+    setTextL("");
+    setTextPool("");
     alert(`Confirmation email has been sent to ${pool}`);
   };
 
   return (
-    <ScrollView style={styles.scrollView}>
-      <View style={styles.container}>
-        <Text style={styles.title}>Registration</Text>
-        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+    <ScrollView style={{ flex: 1 }}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={{ flex: 1 }}
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.container}>
+            <Text style={styles.title}>Registration</Text>
+            <View
+              style={styles.separator}
+              lightColor="#eee"
+              darkColor="rgba(255,255,255,0.1)"
+            />
 
-        <Text style={styles.body}>Parent Name:</Text>
-        <TextInput style={styles.input}
-          placeholder="Full Name"
-          onChangeText={newText => setTextP(newText)}
-          defaultValue={parent}
-          autoFocus={true}
-          returnKeyType="next"
-          onSubmitEditing={() => childInput.current.focus()}
-        />
+            <Text style={styles.body}>Parent Name:</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Full Name"
+              onChangeText={(newText) => setTextP(newText)}
+              defaultValue={parent}
+              autoFocus={true}
+              returnKeyType="next"
+              onSubmitEditing={() => childInput.current.focus()}
+            />
 
-        <Text style={styles.body}>Children Name(s):</Text>
-        <TextInput style={styles.input}
-          placeholder="Full Name(s)"
-          onChangeText={newText => setTextC(newText)}
-          defaultValue={childrens}
-          ref={childInput}
-          returnKeyType="next"
-          onSubmitEditing={() => makeInput.current.focus()}
-        />
+            <Text style={styles.body}>Children Name(s):</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Full Name(s)"
+              onChangeText={(newText) => setTextC(newText)}
+              defaultValue={childrens}
+              ref={childInput}
+              returnKeyType="next"
+              onSubmitEditing={() => makeInput.current.focus()}
+            />
 
-        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-        <Text style={styles.body}>Vehicle Info</Text>
+            <View
+              style={styles.separator}
+              lightColor="#eee"
+              darkColor="rgba(255,255,255,0.1)"
+            />
+            <Text style={styles.body}>Vehicle Info</Text>
 
-        <Text style={styles.body}>Make:</Text>
-        <TextInput style={styles.input}
-          placeholder="EX: Ford/Hyundai/BMW"
-          onChangeText={newText => setTextMake(newText)}
-          defaultValue={make}
-          ref={makeInput}
-          returnKeyType="next"
-          onSubmitEditing={() => modelInput.current.focus()}
-        />
-        
-        <Text style={styles.body}>Model:</Text>
-        <TextInput style={styles.input}
-          placeholder="EX: Explorer/Sonata/X3"
-          onChangeText={newText => setTextModel(newText)}
-          defaultValue={model}
-          ref={modelInput}
-          returnKeyType="next"
-          onSubmitEditing={() => liscenseInput.current.focus()}
-        />
-        <Text style={styles.body}>Liscense Plate:</Text>
-        <TextInput style={styles.input}
-          placeholder="EX: WWW123"
-          onChangeText={newText => setTextL(newText)}
-          defaultValue={liscense}
-          ref={liscenseInput}
-          returnKeyType="next"
-          onSubmitEditing={() => poolInput.current.focus()}
-        />
-        <Text style={styles.body}>Carpool Number:</Text>
-        <TextInput style={styles.input}
-          placeholder="1234"
-          onChangeText={newText => setTextPool(newText)}
-          defaultValue={pool}
-          ref={poolInput}
-          returnKeyType="next"
-          onSubmitEditing={submit}
-        />
+            <Text style={styles.body}>Make:</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="EX: Ford/Hyundai/BMW"
+              onChangeText={(newText) => setTextMake(newText)}
+              defaultValue={make}
+              ref={makeInput}
+              returnKeyType="next"
+              onSubmitEditing={() => modelInput.current.focus()}
+            />
 
-        <View>
-          <Button title="Sign Up" onPress={submit} />
-        </View>
-      </View>
+            <Text style={styles.body}>Model:</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="EX: Explorer/Sonata/X3"
+              onChangeText={(newText) => setTextModel(newText)}
+              defaultValue={model}
+              ref={modelInput}
+              returnKeyType="next"
+              onSubmitEditing={() => liscenseInput.current.focus()}
+            />
+            <Text style={styles.body}>Liscense Plate:</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="EX: WWW123"
+              onChangeText={(newText) => setTextL(newText)}
+              defaultValue={liscense}
+              ref={liscenseInput}
+              returnKeyType="next"
+              onSubmitEditing={() => poolInput.current.focus()}
+            />
+            <Text style={styles.body}>Carpool Number:</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="1234"
+              onChangeText={(newText) => setTextPool(newText)}
+              defaultValue={pool}
+              ref={poolInput}
+              returnKeyType="next"
+              onSubmitEditing={submit}
+            />
+
+            <View>
+              <Button title="Sign Up" onPress={submit} />
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
     </ScrollView>
   );
 }
@@ -101,15 +162,13 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
-  scrollView: {
-    
-  },
+  scrollView: {},
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   body: {
     fontSize: 16,
@@ -117,7 +176,7 @@ const styles = StyleSheet.create({
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
   input: {
     height: 40,
